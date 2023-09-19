fcfb323f9a229454e71a95a1baec85c3718fd971
nimbusaurus@gmail.com <nimbusaurus@gmail.com>
2023-09-19 00:16:05 +0000

    ❇️ New     - Dataset        - tpch-customer        - country-customer-report        - tpch-nation     - Gem        - DatahubTable     - Pipeline        - tpch_examples

---
+++ prophecy/pipelines/tpch_examples/code/tpch_examples/graph/tpch_customer.py
@@ -0,0 +1,10 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+from prophecy.libs import typed_lit
+from tpch_examples.config.ConfigStore import *
+from tpch_examples.udfs.UDFs import *
+
+def tpch_customer(spark: SparkSession) -> DataFrame:
+    return spark.read.table("samples.tpch.customer")
